11683ee61550452893c680ba0fe0697d9b594390
amitsamria <amitsamria@gmail.com>
2024-12-11 00:26:57 -0500

    init

---
+++ src/test/java/com/sam/hellosam/HelloSamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.hellosam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
